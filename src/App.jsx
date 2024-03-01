7f0ecde6c0e843b7b1737f01f93d6f15513f8f5b
Grégoire-cufi Chloé <chloegregoire17@gmail.com>
2024-03-01 16:10:21 +0100

    modif tailwind.config color

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="text-3xl font-bold underline text-green_06">
+      Hello sweetyyy!
     </h1>  )
 }
 
