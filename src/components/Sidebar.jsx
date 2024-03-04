665a8079416877663a9213318b5484f2573f01ce
Grégoire-cufi Chloé <chloegregoire17@gmail.com>
2024-03-04 16:52:09 +0100

    optimisation navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,42 +3,8 @@ import { dataAlbumNav, dataUserNav, imgLogo, styleIcon } from '../constants/appC
 import { NavLink } from 'react-router-dom'
 import { RiCloseLine } from 'react-icons/ri'
 import { HiOutlineMenu } from 'react-icons/hi'
+import NavLinks from './Navlinks'
 
-const NavLinks = (handleClick) => (
-    <>
-    <div className="mt-10">
-        {/* on va mapper sur dataAlbumNav*/ }
-        {dataAlbumNav.map((item)=> (
-            <NavLink
-            key={item.title}
-            to={item.path}
-            end
-            className="flex flex-row p-3 items-center justify-start font-medium text-sm text-white hover:bg-green_06"
-            onClick={()=>handleClick && handleClick()}
-            >
-            <item.icon style={styleIcon} className='mr-2'/>
-            {item.title}
-            </NavLink>
-        ))}
-    </div>
-    <div className="mt-5">
-        {/* on va mapper sur dataAlbumNav*/ }
-        {dataUserNav.map((item)=> (
-            <NavLink
-            key={item.title}
-            to={item.path}
-            end
-            className="flex flex-row p-3 items-center justify-start font-medium text-sm text-white hover:bg-green_06"
-            onClick={()=>handleClick && handleClick()}
-            >
-            <item.icon style={styleIcon} className='mr-2'/>
-            {item.title}
-            </NavLink>
-        ))}
-    </div>
-
-    </>
-)
 
 const Sidebar = () => {
     const [mobileMenu, isMobilMenu] = useState(false);
@@ -48,7 +14,9 @@ const Sidebar = () => {
     {/* navbar pour la ue au dessus de 768px */}
         <div className='hidden md:flex flex-col w-[240px] py-10 px-4 bg-black'>
             <img src={imgLogo} alt="Logo Spotify" className='w-full h-14 object-contain' />
-            <NavLinks />
+            <NavLinks data={dataAlbumNav} marginTop={'mt-10'}/>
+            <NavLinks data={dataUserNav} marginTop={'mt-5'}/>
+
         </div>
         {/* gestion des icones pour ouvrir/fermer le menu en petit ecran*/}
         <div className='absolute md:hidden block top-6 right-3'>
@@ -64,7 +32,9 @@ const Sidebar = () => {
         {/* navbar pour la vue en dessous de 768px */}
         <div className={`z-20 absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white_01 to-black backdrop-blur-lg p-6 md:hidden smooth-transition ${mobileMenu ? 'left-0' : '-left-full'}`}>
         <img src={imgLogo} alt="Logo Spotify" className='w-full h-14 object-contain' />
-        <NavLink handleClick={() => isMobilMenu(false)} />
+        <NavLinks data={dataAlbumNav} marginTop={'mt-10'} handleClick={() => isMobilMenu(false)} />
+        <NavLinks data={dataUserNav} marginTop={'mt-5'} handleClick={() => isMobilMenu(false)}/>
+
         </div>
     </>
 
